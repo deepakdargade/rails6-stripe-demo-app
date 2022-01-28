342fbfb28b99c69363044514177557087f3fcef6
Deepak Dargade <deepak@classpro.in>
2022-01-28 23:43:09 +0530

    second commit

---
+++ app/controllers/webhooks_controller.rb
@@ -0,0 +1,20 @@
+class WebhooksController < ApplicationController
+    # def create
+    #     product = Product.find(params[:id])
+    #     @session = Stripe::Checkout::Session.create({
+    #         payment_method_types: ['card'],
+    #         line_items: [{
+    #             name: product.name,
+    #             amount: product.price * 100,
+    #             currency: 'usd',
+    #             quantity: 1,
+    #         }],
+    #         mode: 'payment',
+    #         success_url: root_url,
+    #         cancel_url: root_url
+    #     })
+    #     respond_to do |format|
+    #         format.js
+    #     end
+    #   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :products
+  resources :webhooks, only: [:create]
   root 'products#index'
   post 'checkout/create', to: 'checkout#create'
 end
