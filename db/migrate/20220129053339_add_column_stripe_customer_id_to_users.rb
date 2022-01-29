72a5b97ce36c1f7915b21d43e3fd393135c10087
Deepak Dargade <deepak@classpro.in>
2022-01-29 11:11:01 +0530

    chore: stripe customer id added in the devise user model

---
+++ db/migrate/20220129053339_add_column_stripe_customer_id_to_users.rb
@@ -0,0 +1,5 @@
+class AddColumnStripeCustomerIdToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :stripe_customer_id, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_052053) do
+ActiveRecord::Schema.define(version: 2022_01_29_053339) do
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2022_01_29_052053) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "stripe_customer_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
